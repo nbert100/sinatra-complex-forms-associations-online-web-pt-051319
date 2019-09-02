37ab7102e5b491097a7165d6531d56f1bbf62302
Nia Bert <nbert14@gmail.com>
2019-09-02 14:14:52 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -11,7 +11,10 @@ class OwnersController < ApplicationController
   end
 
   post '/owners' do 
-     @owner = Owner.create(params[:owner])
+    @owner = Owner.create(params[:owner])
+    if !params["pet"]["name"].empty?
+    @owner.pets << Pet.create(name: params["pet"]["name"])
+    end
     redirect "owners/#{@owner.id}"
   end
 
